9cb5ff2048dbe5162788c7c30ecb6703147db628
Ryan Nagel <RNagel29@gmail.com>
2022-07-09 10:52:14 -0600

    Add sentences 2 & 3

--- variables.js
+++ variables.js
@@ -1,6 +1,6 @@
 var dog = 'Lou'
-var breed = 'Black Lab'
-var passion = 'Play fetch'
+var breed = 'black Lab'
+var passion = 'play fetch'
 var age = 10
 var paws = 4
 var tail = 1
@@ -10,3 +10,13 @@ var meanDemeanor = false
 
 var sentence = `${dog} is a ${breed} who loves to ${passion}`;
 console.log(sentence)
+
+dog = 'Kya'
+breed = 'german shepherd'
+passion = 'eat'
+
+var sentence2 = `${dog} is a ${age} year old ${breed}`;
+console.log(sentence2)
+
+var sentence3 = `${dog} has ${paws} paws and ${tail} tail`;
+console.log(sentence3)
